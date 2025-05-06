a0075dddab1b5c2da4930d6ec49b7ecc52c4b55e
Daniel Adhitthana <dadhitthana@gmail.com>
2025-05-06 09:57:10 +0700

    feat: mobilenetv2

--- models/vision/mobilenet.py
+++ models/vision/mobilenet.py
@@ -28,6 +28,22 @@ class CBR(nn.Sequential):
 			('act', nn.ReLU(inplace=True))
 		]))
 
+class CBR6(nn.Sequential):
+
+	def __init__(
+		self, 
+		in_channels: int, 
+		out_channels: int, 
+		act: Optional[bool] = True,
+		*args, 
+		**kwargs
+	) -> None:
+		super(CBR6, self).__init__(OrderedDict([
+			('conv', nn.Conv2d(in_channels, out_channels, *args, **kwargs)),
+			('norm', nn.BatchNorm2d(out_channels)),
+			('act', nn.ReLU6(inplace=True) if act else nn.Identity())
+		]))
+
 class DWConv(nn.Module):
 
 	def __init__(self, in_channels: int, out_channels: int, *args, **kwargs) -> None:
@@ -40,6 +56,33 @@ class DWConv(nn.Module):
 		x = self.pointwise(x)
 		return x
 
+class Bottleneck(nn.Module):
+
+	def __init__(
+		self, 
+		in_channels: int, 
+		out_channels: int, 
+		stride: Optional[int] = 1, 
+		expansion: Optional[int] = 1, 
+		*args, **kwargs
+	) -> None:
+		super(Bottleneck, self).__init__()
+		self.pointwise = CBR6(in_channels, expansion * in_channels, kernel_size=1)
+		self.depthwise = CBR6(expansion * in_channels, expansion * in_channels, stride=stride, *args, **kwargs)
+		self.linear = CBR6(expansion * in_channels, out_channels, kernel_size=1, act=False)
+		self.shortcut = (
+			CBR6(in_channels, out_channels, kernel_size=1, stride=stride, act=False)
+			if in_channels != out_channels or stride != 1 else nn.Identity()
+		)
+	
+	def forward(self, x: torch.Tensor) -> torch.Tensor:
+		residual = self.shortcut(x)
+		x = self.pointwise(x)
+		x = self.depthwise(x)
+		x = self.linear(x)
+		x += residual
+		return x
+
 @dataclass
 class MobileNetConfig:
 	layers: list[int]
@@ -48,6 +91,7 @@ class MobileNetConfig:
 	num_classes: Optional[int] = 1000
 
 MOBILENET = MobileNetConfig()
+MOBILENETV2 = MobileNetConfig([1, 2, 3, 4, 3, 3, 1])
 
 class MobileNet(nn.Module):
 
@@ -80,8 +124,40 @@ class MobileNet(nn.Module):
 
 class MobileNetV2(nn.Module):
 
-	def __init__(self) -> None:
+	def __init__(self, config: MobileNetConfig) -> None:
 		super(MobileNetV2, self).__init__()
+		self.in_channels = config.base
+		self.features = nn.Sequential(
+			CBR6(config.channels, config.base, kernel_size=3, stride=2, padding=1), # 32
+			self._make_layer(config.base // 2, config.layers[0], expansion=1), # 16
+			self._make_layer(config.base * 3 // 4, config.layers[1], stride=2), # 24
+			self._make_layer(config.base, config.layers[2], stride=2), # 32
+			self._make_layer(config.base * 2, config.layers[3], stride=2), # 64
+			self._make_layer(config.base * 3, config.layers[4]), # 96
+			self._make_layer(config.base * 5, config.layers[5], stride=2), # 160
+			self._make_layer(config.base * 10, config.layers[6]), # 320
+			CBR6(config.base * 10, config.base * 40, kernel_size=1), # 1280
+		)
+		self.fc = nn.Linear(config.base * 40, config.num_classes)
+	
+	def _make_layer(
+		self, 
+		out_channels: int, 
+		depth: int, 
+		stride: Optional[int] = 1, 
+		expansion: Optional[int] = 6
+	) -> nn.Module:
+		layers = [ Bottleneck(self.in_channels, out_channels, kernel_size=3, stride=stride, padding=1, expansion=expansion) ]
+		self.in_channels = out_channels
+		layers += [ Bottleneck(self.in_channels, out_channels, kernel_size=3, padding=1, expansion=expansion) for _ in range(depth - 1) ]
+		return nn.Sequential(*layers)
+	
+	def forward(self, x: torch.Tensor) -> torch.Tensor:
+		x = self.features(x)
+		x = F.adaptive_avg_pool2d(x, 1)
+		x = x.view(x.size(0), -1)
+		x = self.fc(x)
+		return x
 
 class MobileNetV3(nn.Module):
 
